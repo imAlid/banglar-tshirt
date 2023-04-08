edd6d51b8369327ae163d35b10678f3dbf745817
ImAlid <you@example.com>
2023-04-09 00:16:13 +0600

    added cart add and remove option, and added toast

---
+++ src/compu/Cart/Cart.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import TShirt from "../tsHIRT/TShirt";
+
+const Cart = ({ cart, handleRemoveFromCart }) => {
+  return (
+    <div>
+      <h2>Order Summary: {cart.length}</h2>
+      {cart.map((tshirt) => (
+        <p key={tshirt._id}>
+          {tshirt.name}
+          <button 
+          onClick={()=> handleRemoveFromCart(tshirt._id)}
+          >X</button>
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Cart;
